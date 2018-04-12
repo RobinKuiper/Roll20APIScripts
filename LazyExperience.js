6f99d20753f8c410b168750d860a964539cf1f54
Robin Kuiper <robingjkuiper@gmail.com>
2018-04-12 21:41:05 +0200

    Updated Discord username

--- LazyExperience.js
+++ LazyExperience.js
@@ -2,7 +2,7 @@
  * Version 0.1.3
  * Made By Robin Kuiper
  * Skype: RobinKuiper.eu
- * Discord: Robin#1095
+ * Discord: Atheos#1095
  * Roll20: https://app.roll20.net/users/1226016/robin-k
  * Roll20 Thread: https://app.roll20.net/forum/post/6248700/script-beta-beyondimporter-import-dndbeyond-character-sheets
  * Github: https://github.com/RobinKuiper/Roll20APIScripts
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Roll20 API Scripts
 
 * Skype: RobinKuiper.eu
-* Discord: Robin#1095
+* Discord: Atheos#1095
 * Roll20: https://app.roll20.net/users/1226016/robin-k
 * Reddit: https://www.reddit.com/user/robinkuiper/
 
