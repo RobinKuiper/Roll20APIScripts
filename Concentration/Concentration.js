ec9f2003d11c92961eff0553ab5669112048a4a6
Robin Kuiper <robingjkuiper@gmail.com>
2018-04-15 21:28:35 +0200

    First Commit

---
+++ Concentration/Concentration.js
@@ -0,0 +1,172 @@
+/*
+ * Version 0.0.1
+ * Made By Robin Kuiper
+ * Skype: RobinKuiper.eu
+ * Discord: Atheos#1014
+ * Roll20: https://app.roll20.net/users/1226016/robin-k
+ * Github: https://github.com/RobinKuiper/Roll20APIScripts
+ * Reddit: https://www.reddit.com/user/robinkuiper/
+*/
+
+var Concentration = Concentration || (function() {
+    'use strict';
+
+    // Styling for the chat responses.
+    const styles = {
+        reset: 'padding: 0; margin: 0;',
+        menu:  'background-color: #fff; border: 1px solid #000; padding: 5px; border-radius: 5px;',
+        button: 'background-color: #000; border: 1px solid #292929; border-radius: 3px; padding: 5px; color: #fff; text-align: center;',
+        list: 'list-style: none;',
+        float: {
+            right: 'float: right;',
+            left: 'float: left;'
+        },
+        overflow: 'overflow: hidden;',
+        fullWidth: 'width: 100%;',
+        underline: 'text-decoration: underline;',
+        strikethrough: 'text-decoration: strikethrough'
+    },
+    script_name = 'Concentration',
+    state_name = 'CONCENTRATION',
+
+    handleInput = (msg) => {
+        if (msg.type != 'api') return;
+
+        // Split the message into command and argument(s)
+        let args = msg.content.split(' ');
+        let command = args.shift().substring(1);
+        let extracommand = args.shift();
+
+        if (command == state[state_name].config.command) {
+            switch(extracommand){
+                case 'help':
+                    sendHelpMenu();
+                break;
+
+                case 'reset':
+                    state[state_name] = {};
+                    setDefaults(true);
+                    sendConfigMenu();
+                break;
+
+                case 'config':
+                    if(args.length > 0){
+                        let setting = args.shift().split('|');
+                        let key = setting.shift();
+                        let value = (setting[0] === 'true') ? true : (setting[0] === 'false') ? false : setting[0];
+
+                        state[state_name].config[key] = value;
+                    }
+
+                    sendConfigMenu();
+                break;
+
+                default:
+                    sendHelpMenu();
+                break;
+            }
+        }
+    },
+
+    sendConfigMenu = (first) => {
+        let commandButton = makeButton('!'+state[state_name].config.command, '!' + state[state_name].config.command + ' config command|?{Command (without !)}', styles.button + styles.float.right)
+
+        let listItems = [
+            '<span style="'+styles.float.left+'">Command:</span> ' + commandButton,
+        ];
+
+        let resetButton = makeButton('Reset', '!' + state[state_name].config.command + ' reset', styles.button + styles.fullWidth);
+
+        let title_text = (first) ? script_name + ' First Time Setup' : script_name + ' Config';
+        let contents = makeList(listItems, styles.reset + styles.list + styles.overflow, styles.overflow)+'<hr><p style="font-size: 80%">You can always come back to this config by typing `!'+state[state_name].config.command+' config`.</p><hr>'+resetButton;
+        makeAndSendMenu(contents, title_text, 'gm');
+    },
+
+    sendHelpMenu = (first) => {
+        let configButton = makeButton('Config', '!' + state[state_name].config.command + ' config', styles.button + styles.fullWidth)
+
+        let listItems = [
+            '<span style="'+styles.underline+'">!'+state[state_name].config.command+' help</span> - Shows this menu.',
+            '<span style="'+styles.underline+'">!'+state[state_name].config.command+' config</span> - Shows the configuration menu.',
+        ]
+
+        let contents = '<b>Commands:</b>'+makeList(listItems, styles.reset + styles.list)+'<hr>'+configButton;
+        makeAndSendMenu(contents, script_name + ' Help', 'gm')
+    },
+
+    makeAndSendMenu = (contents, title, whisper) => {
+        title = (title && title != '') && makeTitle(title)
+        whisper = (whisper && whisper !== '') && '/w ' + whisper + ' ';
+        sendChat(script_name, whisper + '<div style="'+styles.menu+styles.overflow+'">'+title+contents+'</div>');
+    },
+
+    makeTitle = (title) => {
+        return '<h3 style="margin-bottom: 10px;">'+title+'</h3>';
+    },
+
+    makeButton = (title, href, style) => {
+        return '<a style="'+style+'" href="'+href+'">'+title+'</a>';
+    },
+
+    makeList = (items, listStyle, itemStyle) => {
+        let list = '<ul style="'+listStyle+'">';
+        items.forEach((item) => {
+            list += '<li style="'+itemStyle+'">'+item+'</li>';
+        });
+        list += '</ul>';
+        return list;
+    },
+
+    pre_log = (message) => {
+        log('---------------------------------------------------------------------------------------------');
+        log(message);
+        log('---------------------------------------------------------------------------------------------');
+    },
+
+    checkInstall = () => {
+        if(!_.has(state, state_name)){
+            state[state_name] = state[state_name] || {};
+        }
+        setDefaults();
+
+        log(script_name + ' Ready! Command: !'+state[state_name].config.command);
+        if(state[state_name].config.debug){ makeAndSendMenu(script_name + ' Ready! Debug On.', '', 'gm') }
+    },
+
+    registerEventHandlers = () => {
+        on('chat:message', handleInput);
+    }
+
+    setDefaults = (reset) => {
+        const defaults = {
+            config: {
+                command: 'concentration'
+            }
+        };
+
+        if(!state[state_name].config){
+            state[state_name].config = defaults.config;
+        }else{
+            if(!state[state_name].config.hasOwnProperty('command')){
+                state[state_name].config.command = defaults.config.command;
+            }
+        }
+
+        if(!state[state_name].config.hasOwnProperty('firsttime') && !reset){
+            sendConfigMenu(true);
+            state[state_name].config.firsttime = false;
+        }
+    };
+
+    return {
+        CheckInstall: checkInstall,
+        RegisterEventHandlers: registerEventHandlers
+    }
+})();
+
+on('ready',function() {
+    'use strict';
+
+    Concentration.CheckInstall();
+    Concentration.RegisterEventHandlers();
+});
---
+++ Concentration/README.md
@@ -0,0 +1,17 @@
+# WORK IN PROGRESS
+
+## Concentration
+
+* Skype: RobinKuiper.eu
+* Discord: Atheos#1095
+* Roll20: https://app.roll20.net/users/1226016/robin-k
+* Reddit: https://www.reddit.com/user/robinkuiper/
+
+---
+
+Concentration keeps track of characters concentration, and reminds to do a concentration check. 
+
+### Commands
+
+* **!concentration help** - Shows the help menu.
+* **!concentration config** - Shows the config menu.
