c05ffc16b0d85018dd71593a36b4572edd7f89ed
Robin Kuiper <robingjkuiper@gmail.com>
2018-11-01 10:04:25 +0100

    Possibility to add a number to a statusmarker

--- StatusInfo/StatusInfo.js
+++ StatusInfo/StatusInfo.js
@@ -1,5 +1,5 @@
 /*
- * Version: 0.3.10
+ * Version: 0.3.11
  * Made By Robin Kuiper
  * Skype: RobinKuiper.eu
  * Discord: Atheos#1095
@@ -39,7 +39,7 @@ var StatusInfo = StatusInfo || (function() {
         };
 
     
-    const version = "0.3.8",
+    const version = "0.3.11",
     
     // Styling for the chat responses.
     style = "overflow: hidden; background-color: #fff; border: 1px solid #000; padding: 5px; border-radius: 5px;",
@@ -142,7 +142,8 @@ var StatusInfo = StatusInfo || (function() {
                         state[state_name].conditions[condition.toLowerCase()] = {
                             name: condition,
                             icon: 'red',
-                            description: ''
+                            description: '',
+                            number: 0
                         }
 
                         sendSingleConditionConfigMenu(condition.toLowerCase());
@@ -182,6 +183,8 @@ var StatusInfo = StatusInfo || (function() {
 
                             // If we are editting the description, join the args all together in a string.
                             value = (key === 'description') ? value + ' ' + args.join(' ') : value;
+                            // If we are editting the number, save it as integer.
+                            value = (key === 'number') ? parseInt(value) : value;
 
                             state[state_name].conditions[condition][key] = value;
                         }
@@ -240,11 +243,12 @@ var StatusInfo = StatusInfo || (function() {
             }
 
             condition_key = condition_key.toLowerCase();
+            let condition = getConditionByName(condition_key);
 
             tokens.forEach(token => {
                 let prevSM = token.get('statusmarkers');
-                let add = (type === 'add') ? true : (type === 'toggle') ? !token.get('status_'+getConditionByName(condition_key).icon) : false;
-                token.set('status_'+getConditionByName(condition_key).icon, add);
+                let add = (type === 'add') ? true : (type === 'toggle') ? !token.get('status_'+condition.icon) : false;
+                token.set('status_'+condition.icon, (add) ? (condition.number) ? condition.number : add : false);
 
                 let prev = token;
                 prev.attributes.statusmarkers = prevSM;
@@ -252,7 +256,7 @@ var StatusInfo = StatusInfo || (function() {
                 notifyObservers('tokenChange', token, prev);
 
                 if(add && !handled.includes(condition_key)){
-                    sendConditionToChat(getConditionByName(condition_key));
+                    sendConditionToChat(condition);
                     doHandled(condition_key);
                 }
 
@@ -470,6 +474,15 @@ var StatusInfo = StatusInfo || (function() {
         let markerButton = makeButton(getIcon(condition.icon) || condition.icon, '!' + state[state_name].config.command + ' config-conditions '+conditionKey+' icon|'+markerDropdown, buttonStyle);
         listItems.push('<span style="float: left">Statusmarker: </span> ' + markerButton);
 
+        let numberDropdown = '?{Number';
+        for(var i = 0; i <= 9; i++){
+            numberDropdown += '|'+i;
+        }
+        numberDropdown += '}';
+
+        let numberButton = makeButton(condition.number || "none", '!' + state[state_name].config.command + ' config-conditions ' +conditionKey+' number|'+numberDropdown, buttonStyle);
+        listItems.push('<span style="float: left">Number: </span> ' + numberButton);
+
         let backButton = makeButton('Back', '!' + state[state_name].config.command + ' config-conditions', buttonStyle + ' width: 100%');
         let removeButton = makeButton('Remove', '!' + state[state_name].config.command + ' config-conditions remove '+conditionKey+' ?{Are you sure?|Yes,yes|No,no}', buttonStyle + ' width: 100%');
         let changeButton = makeButton('Edit Description', '!' + state[state_name].config.command + ' config-conditions '+conditionKey+' description|?{Description|'+condition.description+'}', buttonStyle);
@@ -656,77 +669,92 @@ var StatusInfo = StatusInfo || (function() {
                 blinded: {
                     name: 'Blinded',
                     description: '<p>A blinded creature can’t see and automatically fails any ability check that requires sight.</p> <p>Attack rolls against the creature have advantage, and the creature’s Attack rolls have disadvantage.</p>',
-                    icon: 'bleeding-eye'
+                    icon: 'bleeding-eye',
+                    number: 0
                 },
                 charmed: {
                     name: 'Charmed',
                     description: '<p>A charmed creature can’t Attack the charmer or target the charmer with harmful Abilities or magical effects.</p> <p>The charmer has advantage on any ability check to interact socially with the creature.</p>',
-                    icon: 'broken-heart'
+                    icon: 'broken-heart',
+                    number: 0
                 },
                 deafened: {
                     name: 'Deafened',
                     description: '<p>A deafened creature can’t hear and automatically fails any ability check that requires hearing.</p>',
-                    icon: 'edge-crack'
+                    icon: 'edge-crack',
+                    number: 0
                 },
                 frightened: {
                     name: 'Frightened',
                     description: '<p>A frightened creature has disadvantage on Ability Checks and Attack rolls while the source of its fear is within line of sight.</p> <p>The creature can’t willingly move closer to the source of its fear.</p>',
-                    icon: 'screaming'
+                    icon: 'screaming',
+                    number: 0
                 },
                 grappled: {
                     name: 'Grappled',
                     description: '<p>A grappled creature’s speed becomes 0, and it can’t benefit from any bonus to its speed.</p> <p>The condition ends if the Grappler is <i>incapacitated</i>.</p> <p>The condition also ends if an effect removes the grappled creature from the reach of the Grappler or Grappling effect, such as when a creature is hurled away by the Thunderwave spell.</p>',
-                    icon: 'grab'
+                    icon: 'grab',
+                    number: 0
                 },
                 incapacitated: {
                     name: 'Incapacitated',
                     description: '<p>An incapacitated creature can’t take actions or reactions.</p>',
-                    icon: 'interdiction'
+                    icon: 'interdiction',
+                    number: 0
                 },
                 inspiration: {
                     name: 'Inspiration',
                     description: '<p>If you have inspiration, you can expend it when you make an Attack roll, saving throw, or ability check. Spending your inspiration gives you advantage on that roll.</p> <p>Additionally, if you have inspiration, you can reward another player for good roleplaying, clever thinking, or simply doing something exciting in the game. When another player character does something that really contributes to the story in a fun and interesting way, you can give up your inspiration to give that character inspiration.</p>',
-                    icon: 'black-flag'
+                    icon: 'black-flag',
+                    number: 0
                 },
                 invisibility: {
                     name: 'Invisibility',
                     description: '<p>An invisible creature is impossible to see without the aid of magic or a Special sense. For the purpose of Hiding, the creature is heavily obscured. The creature’s location can be detected by any noise it makes or any tracks it leaves.</p> <p>Attack rolls against the creature have disadvantage, and the creature’s Attack rolls have advantage.</p>',
-                    icon: 'ninja-mask'
+                    icon: 'ninja-mask',
+                    number: 0
                 },
                 paralyzed: {
                     name: 'Paralyzed',
                     description: '<p>A paralyzed creature is <i>incapacitated</i> and can’t move or speak.</p> <p>The creature automatically fails Strength and Dexterity saving throws.</p> <p>Attack rolls against the creature have advantage.</p> <p>Any Attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.</p>',
-                    icon: 'pummeled'
+                    icon: 'pummeled',
+                    number: 0
                 },
                 petrified: {
                     name: 'Petrified',
                     description: '<p>A petrified creature is transformed, along with any nonmagical object it is wearing or carrying, into a solid inanimate substance (usually stone). Its weight increases by a factor of ten, and it ceases aging.</p> <p>The creature is <i>incapacitated</i>, can’t move or speak, and is unaware of its surroundings.</p> <p>Attack rolls against the creature have advantage.</p> <p>The creature automatically fails Strength and Dexterity saving throws.</p> <p>The creature has Resistance to all damage.</p> <p>The creature is immune to poison and disease, although a poison or disease already in its system is suspended, not neutralized.</p>',
-                    icon: 'frozen-orb'
+                    icon: 'frozen-orb',
+                    number: 0
                 },
                 poisoned: {
                     name: 'Poisoned',
                     description: '<p>A poisoned creature has disadvantage on Attack rolls and Ability Checks.</p>',
-                    icon: 'chemical-bolt'
+                    icon: 'chemical-bolt',
+                    number: 0
                 },
                 prone: {
                     name: 'Prone',
                     description: '<p>A prone creature’s only Movement option is to crawl, unless it stands up and thereby ends the condition.</p> <p>The creature has disadvantage on Attack rolls.</p> <p>An Attack roll against the creature has advantage if the attacker is within 5 feet of the creature. Otherwise, the Attack roll has disadvantage.</p>',
-                    icon: 'back-pain'
+                    icon: 'back-pain',
+                    number: 0
                 },
                 restrained: {
                     name: 'Restrained',
                     description: '<p>A restrained creature’s speed becomes 0, and it can’t benefit from any bonus to its speed.</p> <p>Attack rolls against the creature have advantage, and the creature’s Attack rolls have disadvantage.</p> <p>The creature has disadvantage on Dexterity saving throws.</p>',
-                    icon: 'fishing-net'
+                    icon: 'fishing-net',
+                    number: 0
                 },
                 stunned: {
                     name: 'Stunned',
                     description: '<p>A stunned creature is <i>incapacitated</i>, can’t move, and can speak only falteringly.</p> <p>The creature automatically fails Strength and Dexterity saving throws.</p> <p>Attack rolls against the creature have advantage.</p>',
-                    icon: 'fist'
+                    icon: 'fist',
+                    number: 0
                 },
                 unconscious: {
                     name: 'Unconscious',
                     description: '<p>An unconscious creature is <i>incapacitated</i>, can’t move or speak, and is unaware of its surroundings.</p> <p>The creature drops whatever it’s holding and falls prone.</p> <p>The creature automatically fails Strength and Dexterity saving throws.</p> <p>Attack rolls against the creature have advantage.</p> <p>Any Attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.</p>',
-                    icon: 'sleepy'
+                    icon: 'sleepy',
+                    number: 0
                 },
             },
         };
